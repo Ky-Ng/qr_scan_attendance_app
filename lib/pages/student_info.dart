84da4b02a7f1c15ff253b77af61c22f2067d7873
kyleng <ng.kyle101@gmail.com>
2022-08-02 23:22:44 -0700

    Add initial code structure

---
+++ lib/pages/student_info.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class StudentInfo extends StatefulWidget {
+  const StudentInfo({Key? key}) : super(key: key);
+
+  @override
+  State<StudentInfo> createState() => _StudentInfoState();
+}
+
+class _StudentInfoState extends State<StudentInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
